bd3219887c77a36f2aab93f55afd191927a4b4af
Ashish Pal <ashishpal1717@gmail.com>
2023-05-04 00:02:16 +0530

    login, signup, logout, blacklisting done

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,22 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const {mongoDB}=require('./config/db');
+const {client}=require('./config/db');
 const {userRouter}=require('./routes/userRouter');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors({origin: "*"}));
-
 app.use("/users", userRouter);
-
 
 app.listen(process.env.port, async () => {
   try {
+    // mongoDb connection
     await mongoDB;
-    console.log(`Running at PORT:${process.env.port}`);
-  } catch (error) {
+    console.log('MongoDB', true);
+
+    // redis connection
+    await client.connect();
+    console.log('Redis', client.isReady);
+  } 
+  catch (error) {
     console.log(error);
   }
+  console.log(`Running at:${process.env.port}`);
 });
