82c66fa0483f50d0df471be1e802862f8ff8b134
vishalpadaswan3 <vishalpadaswan1.com>
2023-05-04 22:49:11 +0530

    homepage

---
+++ FRONTEND/1.js
@@ -0,0 +1,20 @@
+const leftScroll = document.querySelector('.left-scroll');
+const rightScroll = document.querySelector('.right-scroll');
+const scrollContainer = document.querySelector('.scroll-container');
+
+let currentIndex = 0;
+const scrollAmount = 1;
+const images = scrollContainer.querySelectorAll('img');
+const maxIndex = images.length - 1;
+
+leftScroll.addEventListener('click', () => {
+  currentIndex = (currentIndex - scrollAmount < 0) ? maxIndex : currentIndex - scrollAmount;
+  const scrollPosition = images[currentIndex].offsetLeft;
+  scrollContainer.style.transform = `translateX(-${scrollPosition}px)`;
+});
+
+rightScroll.addEventListener('click', () => {
+  currentIndex = (currentIndex + scrollAmount > maxIndex) ? 0 : currentIndex + scrollAmount;
+  const scrollPosition = images[currentIndex].offsetLeft;
+  scrollContainer.style.transform = `translateX(-${scrollPosition}px)`;
+});
