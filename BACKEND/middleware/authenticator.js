cff339a80305dd94b72164f78abe21fa23f04669
sauravs4h <sauravs4h@gmail.com>
2023-05-06 08:51:14 +0530

    edit-2

--- BACKEND/middleware/authenticator.js
+++ BACKEND/middleware/authenticator.js
@@ -8,29 +8,28 @@ const authenticator= async(req,res,next)=>{
         if(accessToken){
             const isBlacklisted=await client.SISMEMBER('blackTokens',accessToken);
             if(isBlacklisted){
-                res.status(401).send('Blacklisted Token..!');
+                res.status(401).send({msg:'Blacklisted Token..!',status:error});
             }
             else{
                 jwt.verify(accessToken,process.env.normalKey,(err,decoded)=>{
                     if(err){
                         return res.send({msg:err.message,status:"error"})
                     }
-                    if(decoded){
-                        res.body=decoded;
-                        next();
-                    }
                     else{
-                        res.status(401).send('Invalid Token..!');
+                        let userID=decoded.userID;
+                        req.body.userID=userID
+                        next()
                     }
+                    
                 })
             }
         }
         else{
-            res.status(401).send('Please login first..!');
+            res.status(401).send({msg:'Please login first..!',status:error});
         }
     } catch (error) {
         // console.log(error);
-        res.sendStatus(400);
+        res.status(401).send({msg:'Something went wrong!',status:error});
     }
 }
 
