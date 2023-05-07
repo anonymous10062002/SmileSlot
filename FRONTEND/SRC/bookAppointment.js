5bc8d3f1e7b0cc2cab118718766cbf4740426ad8
sauravs4h <sauravs4h@gmail.com>
2023-05-07 16:41:27 +0530

    book appointment

--- FRONTEND/SRC/bookAppointment.js
+++ FRONTEND/SRC/bookAppointment.js
@@ -1,4 +1,4 @@
-const token = sessionStorage.getItem("token");
+const token = localStorage.getItem("token");
 const fetchCity = async () => {
   let res = await fetch("http://localhost:4000/users/allcities", {
     method:"GET",
@@ -8,7 +8,7 @@ const fetchCity = async () => {
     }
   });
   let cityData = await res.json();
-  // console.log(cityData)
+   console.log(cityData)
   const citySelect = document.querySelector("#city");
   cityData.map((city)=>{
     citySelect.innerHTML = `
@@ -21,7 +21,7 @@ const fetchCity = async () => {
   })
 
   const fetchClinic = async (city) => {
-    const response = await fetch(`http://localhost:4000/clinic/:${city}`, {
+    const response = await fetch(`http://localhost:4000/users/clinic/${city}`, {
       method: "GET",
       headers: {
         'Content-Type':"application/json",
@@ -29,10 +29,32 @@ const fetchCity = async () => {
       }
     });
     const clinicData = await response.json();
-    console.log(clinicData)
+    console.log("clini",clinicData)
+
+
+
+    const clinicSelect = document.querySelector("#clinic");
+    clinicData.map((el)=>{
+      let option = document.createElement("option");
+      option.value=el._id;
+      option.text=el.clinic;
+
+      clinicSelect.appendChild(option);
+    // clinicSelect.innerHTML = `
+    // <option value="">Select City</option>
+    // <option value="${el._id}">${el.clinic}</option>
+    // `
+  });
+  citySelect.addEventListener("change", () => {
+    
+  })
   }
 }
 window.onload = fetchCity();
+
+
+
+
 
 const appointmentForm = document.querySelector("form");
 appointmentForm.addEventListener("submit", (e) => {
@@ -61,6 +83,10 @@ appointmentForm.addEventListener("submit", (e) => {
     userDetails.text_msg
   );
 });
+  
+
+
+
 
 const appointmentData = async (
   city,
@@ -71,17 +97,24 @@ const appointmentData = async (
   email,
   text_msg
 ) => {
-  const appointData = {city,clinic,date,mobile_number,name,email,text_msg}
-  const response = fetch("http://localhost:4000/users/bookslot", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`
-    },
-    body: JSON.stringify(appointData)
-  })
-  if(response.status){
-    alert("Slot Booked");
-  }
-  console.log(city, clinic, date, mobile_number, name, email, text_msg);
+  //let appointData = {city,clinic,date,mobile_number,name,email,text_msg}
+  let appointData = {date}
+
+  let clinic_id=  document.querySelector("#clinic").value
+
+  console.log(clinic_id,appointData)
+  //console.log("abc",appointData)
+   const response = await fetch(`http://localhost:4000/users/bookslot/${clinic_id}`, {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+       Authorization: `Bearer ${token}`
+     },
+     body: JSON.stringify(appointData)
+   })
+   
+   let result=await response.json();
+   alert(result.msg)
+   console.log(result);
+  // console.log(city, clinic, date, mobile_number, name, email, text_msg);
 };
