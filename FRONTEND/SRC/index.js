6aab8b1a3056c0b501a1b416335c53e453b2bd96
tigmansh <gargtigmanshu@gmail.com>
2023-05-08 16:36:25 +0530

    Implemented Admin Route For Blocking And Deleteing Clinics

--- FRONTEND/SRC/index.js
+++ FRONTEND/SRC/index.js
@@ -1,57 +1,57 @@
-const leftScroll = document.querySelector(".left-scroll");
-const rightScroll = document.querySelector(".right-scroll");
-const scrollContainer = document.querySelector(".scroll-container");
+const leftScroll = document.querySelector('.left-scroll');
+const rightScroll = document.querySelector('.right-scroll');
+const scrollContainer = document.querySelector('.scroll-container');
 
 let currentIndex = 0;
 const scrollAmount = 1;
-const images = scrollContainer.querySelectorAll("img");
+const images = scrollContainer.querySelectorAll('img');
 const maxIndex = images.length - 1;
 
-leftScroll.addEventListener("click", () => {
-  currentIndex =
-    currentIndex - scrollAmount < 0 ? maxIndex : currentIndex - scrollAmount;
+leftScroll.addEventListener('click', () => {
+  currentIndex = (currentIndex - scrollAmount < 0) ? maxIndex : currentIndex - scrollAmount;
   const scrollPosition = images[currentIndex].offsetLeft;
   scrollContainer.style.transform = `translateX(-${scrollPosition}px)`;
 });
 
-rightScroll.addEventListener("click", () => {
-  currentIndex =
-    currentIndex + scrollAmount > maxIndex ? 0 : currentIndex + scrollAmount;
+rightScroll.addEventListener('click', () => {
+  currentIndex = (currentIndex + scrollAmount > maxIndex) ? 0 : currentIndex + scrollAmount;
   const scrollPosition = images[currentIndex].offsetLeft;
   scrollContainer.style.transform = `translateX(-${scrollPosition}px)`;
 });
 
-let usernamehere = document.getElementById("usernamehere");
 
-usernamehere.innerText = localStorage.getItem("dentalusername");
 
-let btnlogin = document.getElementById("btnlogin");
-let token = localStorage.getItem("dentaltoken");
+let usernamehere = document.getElementById("usernamehere")
 
-window.addEventListener("load", () => {
-  if (token == undefined) {
-    window.location.href = "../Public/index.html";
-  }
-});
+usernamehere.innerText = localStorage.getItem("dentalusername")
+
+let btnlogin = document.getElementById("btnlogin")
+let token = localStorage.getItem("dentaltoken")
+
+window.addEventListener("load",()=>{
+    if(token == undefined){
+      window.location.href = "../Public/index.html"
+    }
+})
+
+
 
 btnlogin.addEventListener("click", () => {
   fetch(`http://localhost:4000/users/logout`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      Authorization: token,
-    },
-  })
-    .then((res) => res.json())
+      Authorization: token
+    }
+  }).then((res) => res.json())
     .then((res) => {
       swal(``, res.msg, "success");
       setTimeout(() => {
         localStorage.removeItem("dentaltoken");
         token = null;
         window.location.href = "../Public/index.html";
-      }, 3000);
+      }, 3000)
+    }).catch((error) => {
+      swal(``, error.message, "error")
     })
-    .catch((error) => {
-      swal(``, error.message, "error");
-    });
-});
+})
