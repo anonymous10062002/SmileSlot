f93b142254586808c275bbba70a8c7044c1b9107
Ashish Pal <ashishpal1717@gmail.com>
2023-05-13 15:54:24 +0530

    Update SmileSlot.ts

--- System Design/SmileSlot.ts
+++ System Design/SmileSlot.ts
@@ -1,59 +1,60 @@
-// interface Clinic {
-//     name: string;
-//     price: number;
-// }
 
-// interface Dentists {
-//     name: string;
-//     speciality: string;
-//     availableTime: Date[];
-// }
+interface Clinic {
+name: string;
+price: number;
+}
 
-// interface Appointments {
-//     dentist: string;
-//     tooth: Dentist;
-//     time: Date;
-// }
+interface Dentists {
+name: string;
+speciality: string;
+availableTime: Date[];
+}
 
-// class Dentist {
-//     private dentist: Dentist;
-//     private clinic: Clinic;
+interface Appointments {
+dentist: string;
+tooth: Dentist;
+time: Date;
+}
 
-//     constructor(dentist: Dentist, clinic: Clinic){
-//         this.dentist = dentist;
-//         this.clinic = clinic;
-//     }
+class Dentist {
+private dentist: Dentist;
+private clinic: Clinic;
 
-//     public getDentists(): Dentists[]{
-//         return this.dentist;
-//     }
+constructor(dentist: Dentist, clinic: Clinic){
+this.dentist = dentist;
+this.clinic = clinic;
+}
 
-//     public getClinic(): Clinic[]{
-//         return this.clinic;
-//     }
+public getDentists(): Dentists[]{
+return this.dentist;
+}
 
-//     public findAvailableDentists(time: Date): Dentists[] {
-//         return this.dentist.filter((s) => s.availableTimes.filter(el=>el.getTime()-time.getTime()===0));
-//       }
+public getClinic(): Clinic[]{
+return this.clinic;
+}
+
+public findAvailableDentists(time: Date): Dentists[] {
+return this.dentist.filter((s) => s.availableTimes.filter(el=>el.getTime()-time.getTime()===0));
+}
 
 
-//       public bookAppointment(dentist: Dentist, clinic: Clinic, time: Date): Appointment {
-//         // Check if the stylist is available at the specified time
-//         if (!this.dentist.filter((s) => s.availableTimes.filter(el=>el.getTime()-time.getTime()===0))) {
-//           throw new Error(`Stylist ${dentist.name} is not available at ${time}`);
-//         }
+public bookAppointment(dentist: Dentist, clinic: Clinic, time: Date): Appointment {
+// Check if the stylist is available at the specified time
+if (!this.dentist.filter((s) => s.availableTimes.filter(el=>el.getTime()-time.getTime()===0))) {
+throw new Error(`Stylist ${dentist.name} is not available at ${time}`);
+}
     
-//         // Remove the time from the dentist's available times
-//         dentist.availableTimes = dentist.availableTimes.filter((t) => t !== time);
+// Remove the time from the dentist's available times
+dentist.availableTimes = dentist.availableTimes.filter((t) => t !== time);
     
-//         // Create the appointment object
-//         const appointment: Appointment = {
-//           dentist,
-//           clinic,
-//           time,
-//         };
+// Create the appointment object
+const appointment: Appointment = {
+dentist,
+clinic,
+time,
+};
     
-//         return appointment;
-//       }
+return appointment;
+}
     
-// }
+}
